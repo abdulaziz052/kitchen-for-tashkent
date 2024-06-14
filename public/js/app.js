ecca0fc240639f760aeac42958d59bde2c2b70a0
abdulaziz052 <killerabdulaziz60@gmail.com>
2024-06-14 11:49:37 +0500

    add

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,15 @@ box.forEach(function (item, index) {
     })
 })
 
+
+
+let box2 = document.querySelectorAll('#activeBox2')
+box2.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        box2.forEach(function (item, index) {
+            item.classList.remove('active2')
+        })
+        item.classList.add('active2')
+    })
+})
+
