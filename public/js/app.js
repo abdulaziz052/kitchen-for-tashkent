ae3b1ad08f75ac40aabb22fa6e648fdace5ee801
abdulaziz052 <killerabdulaziz60@gmail.com>
2024-06-14 12:01:18 +0500

    adas

--- public/js/app.js
+++ public/js/app.js
@@ -20,3 +20,14 @@ box2.forEach(function (item, index) {
     })
 })
 
+let box3 = document.querySelectorAll('#activeBox3')
+box3.forEach(function (item, index) {
+    item.addEventListener('click', function () {
+        box3.forEach(function (item, index) {
+            item.classList.remove('active3')
+        })
+        item.classList.add('active3')
+    })
+})
+
+
